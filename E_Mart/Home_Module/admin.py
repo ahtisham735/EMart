a1d757171699394f86ec91518dec4b48ee1d8d0e
Ahtisham <sahtisham735@gmail.com>
2021-03-16 10:34:02 +0500

    Database Created

--- E_Mart/Home_Module/admin.py
+++ E_Mart/Home_Module/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from Home_Module.models import *
 
 # Register your models here.
-class RegisterCustomer:
+class RegisterCustomer(admin.ModelAdmin):
     list_display=("email","username","password")
+
+admin.site.register(Customer,RegisterCustomer)
