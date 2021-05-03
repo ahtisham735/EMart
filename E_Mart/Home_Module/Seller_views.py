7eed77837a3481e75de42ddf8cbe3c468995a970
MCSF19M052 <MCSF19M052@pucit.edu.pk>
2021-05-03 16:24:00 +0500

    Detail update

--- E_Mart/Home_Module/Seller_views.py
+++ E_Mart/Home_Module/Seller_views.py
@@ -39,6 +39,19 @@ def add_product(request):
         HttpResponseRedirect(reverse("Home_Module/seller_center"))
     return render(request,"Seller_Module/addproduct.html",context={"user":user})
 
-
+def seller_detail_update(request):
+    user=isUserLogin(request,'seller')
+    detail=SellerDetail.objects.get(pk=user.id)
+    if request.method=="GET":
+        if user is None:
+            return HttpResponseRedirect(reverse("Home_Module:seller_center"))     
+        return render(request,"Seller_Module/SellerDetailUpdate.html",context={"detail":detail})
+    if request.method=="POST":
+        detail.shop=request.POST['shop']
+        detail.cnic=request.POST['cnic']
+        detail.phone=request.POST['phone']
+        detail.account_no=request.POST['account']
+        detail.address=request.POST['address']
+        detail.save()
+        return render(request,"Seller_Module/Seller_base.html",context={"user":user})
         
-   
