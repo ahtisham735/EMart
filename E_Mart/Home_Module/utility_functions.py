3600ffb779a9642036f94d9fc1c82c506d682d43
Ahtisham <sahtisham735@gmail.com>
2021-05-19 16:41:55 +0500

    farmaishy push

--- E_Mart/Home_Module/utility_functions.py
+++ E_Mart/Home_Module/utility_functions.py
@@ -1,7 +1,18 @@
 from .models import User
+from django.contrib import auth,messages
 def isUserLogin(request,name):
     if name in request.session:
         user=User.objects.get(username=request.session[name])
         return user
     else:
         return None
+def permission_check(request):
+    user=isUserLogin(request,'user')
+    if user is None:
+        user=request.user
+    if not user.is_authenticated or user.is_admin:
+        messages.error(request,"You must login first to access this page")
+        return None
+    else:
+        return user
+     
